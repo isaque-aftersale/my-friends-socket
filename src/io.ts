473c3f46ba399d6ce2a3fdc787254be38edb3023
Isaque Claudino <isaque.santos@after.sale>
2024-02-18 18:19:14 -0300

    feat: created server

--- src/io.ts
+++ src/io.ts
@@ -1,15 +1,33 @@
+import express from "express";
 import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
 
-export const io = new Server({
+export const app = express();
+
+export const httpServer = createServer(app);
+
+export const io = new Server(httpServer, {
   cors: {
     allowedHeaders: ["Access-Control-Allow-Headers: all"],
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("Connection ok");
-  
-  io.emit("test", "This is message test");
+app.use(
+  cors({
+    allowedHeaders: ["Access-Control-Allow-Headers: all"],
+  })
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "all ok",
+  });
 });
 
-io.listen(3003);
+io.on("connection", (socket) => {
+  console.log("Connection ok");
+});
+
+httpServer.listen(3003);
