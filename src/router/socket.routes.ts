3f9e41b61b09a5a5afc2e720a497130b571e48ec
Isaque Claudino <isaque.santos@after.sale>
2024-02-19 15:42:26 -0300

    feat: created add contact

--- src/router/socket.routes.ts
+++ src/router/socket.routes.ts
@@ -1,11 +1,21 @@
+import uuid from 'uuid';
+
 import { io } from '../server';
 
 io.on("connection", (socket) => {
   console.log("Connected -> " + socket.id);
 
-  socket.on('create chat', (data) => {
-    socket.join('')
-  })
+  socket.on("user-create-connection", ({ id, name }) => {
+    socket.emit("user-create-connection", {
+      id: socket.id,
+      connected: true,
+      name,
+    });
+  });
+
+  socket.on("user-save-contact", (contact) => {
+    socket.emit("user-save-contact", contact);
+  });
 
   socket.on("disconnect", (data) => {
     console.log("Disconected -> " + data);
